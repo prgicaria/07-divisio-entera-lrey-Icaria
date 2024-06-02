6a0074bf3fa0a65942809634d0df1fd3ab244247
Luis <lrey@xtec.cat>
2024-06-02 10:29:44 +0200

    Inicial 2024-25

---
+++ divisio.py
@@ -0,0 +1,9 @@
+print("Divisió de nombres enters")
+dividend = int(input("Entra el dividend: "))
+divisor = int(input("Entra el divisor: "))
+
+quocient = dividend//divisor
+residu = dividend % divisor
+print("Divisió: {dividend}/{divisor}")
+print("Quocient: {quocient}")
+print("Residu: {residu}")
